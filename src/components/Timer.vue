0d1300a5b3f8fbb08c619ed3584d69d3d16c121c
Diptangshu Dey <dipdeys.dey@gmail.com>
2022-02-01 01:39:01 +0530

    fixed timer and file upload

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,104 +1,106 @@
 <template>
-<v-container class="d-flex justify-end">
-  <div class="block" v-if="days > 0">
-    <p class="digit">{{ days | two_digits }}</p>
-    <p class="text">Days</p>
-  </div>
-  <div class="block" v-if="hours > 0 ">
-    <p class="digit">{{ hours | two_digits }}</p>
-    <p class="text">Hours</p>
-  </div>
-  <div  class="block">
-    <p class="digit">{{ minutes | two_digits }}</p>
-    <p class="text">Minutes</p>
-  </div>
-  <div  class="block">
-    <p class="digit">{{ seconds | two_digits }}</p>
-    <p class="text">Seconds</p>
-  </div>
-  <!-- <div v-if="stop" class="text-lg-h5">Time Up!</div> -->
-</v-container>
+  <v-container class="d-flex justify-end">
+    <!-- <div class="block" v-if="days > 0">
+      <p class="digit">{{ days | two_digits }}</p>
+      <p class="text">Days</p>
+    </div>
+    <div class="block" v-if="hours > 0">
+      <p class="digit">{{ hours | two_digits }}</p>
+      <p class="text">Hours</p>
+    </div>-->
+    <div class="block" v-if="!stop">
+      <p class="digit">{{ minutes | two_digits }}</p>
+      <p class="text">Minutes</p>
+    </div>
+    <div class="block" v-if="!stop">
+      <p class="digit">{{ seconds | two_digits }}</p>
+      <p class="text">Seconds</p>
+    </div>
+    <div v-if="stop" class="text-lg-h5">Time Up!</div>
+  </v-container>
 </template>
 <script>
 export default {
-    name: 'Timer',
-    mounted() {
-        window.setInterval(() => {
-            this.now = Math.trunc((new Date()).getTime() / 1000);
-        },1000);
-    },
-    props : {
-      date : {
-        type: Number,
-        // coerce: num => Math.trunc(num / 1000)
-        require: true,
-      },
-      mobileView: Boolean,
-    },
-    data() {
-      return {
-        now: this.now = Math.trunc((new Date()).getTime() / 1000),
-        // stop: this.timeUp(),
-      }
-    },
-    computed: {
-        seconds() {
-            return Math.trunc((this.date - this.now) % 60);
-        },
-        minutes() {
-            return Math.trunc((this.date - this.now) / 60) % 60;
-        },
-        hours() {
-            return Math.trunc((this.date - this.now) / 60 / 60) % 24;
-        },
-        days() {
-            return Math.trunc((this.date - this.now) / 60 / 60 / 24);
-        },
-        // timeUp() {
-        //   if(this.date - this.now <= 0){
-        //     return true;
-        //   }else{
-        //     return false;
-        //   }
-        // }
-    },
-    methods: {
+  name: 'Timer',
+  mounted() {
+    this.startTimer();
+  },
+  props: ["time", "mobileView"],
+  data() {
+    return {
+      timePassed: 0,
+      timerInterval: null,
+      stop: false,
     }
+  },
+  computed: {
+    seconds() {
+      return Math.trunc(this.timeLeft % 60);
+    },
+    minutes() {
+      return Math.trunc(this.timeLeft / 60);
+    },
+    // hours() {
+    //   return Math.trunc((this.date - this.now) / 60 / 60) % 24;
+    // },
+    // days() {
+    //   return Math.trunc((this.date - this.now) / 60 / 60 / 24);
+    // },
+    timeLeft() {
+      return this.time - this.timePassed;
+    }
+  },
+  watch: {
+    timeLeft(newValue) {
+      if (newValue === 0) {
+        this.timeUp();
+        this.stop = true;
+      }
+    }
+  },
+  methods: {
+    timeUp() {
+      clearInterval(this.timerInterval);
+    },
+    startTimer() {
+      this.timerInterval = setInterval(() => (this.timePasses += 1), 1000);
+    },
+  }
 }
 </script>
 <style>
 @import url(https://fonts.googleapis.com/css?family=Roboto+Condensed:400|Roboto:100);
 .block {
-    display: flex;
-    flex-direction: column;
-    margin: 10px;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
 }
 .text {
-    color: #1abc9c;
-    font-size: 13px;
-    font-family: 'Roboto Condensed', serif;
-    font-weight: bolder;
-    margin-top:0px;
-    margin-bottom: 5px;
-    text-align: center;
+  color: #1abc9c;
+  font-size: 13px;
+  font-family: "Roboto Condensed", serif;
+  font-weight: bolder;
+  margin-top: 0px;
+  margin-bottom: 5px;
+  text-align: center;
 }
 .digit {
-    color: #ecf0f1;
-    font-size: 40px;
-    font-weight: regular;
-    font-family: 'Roboto', serif;
-    margin: 2px;
-    margin-bottom: 0px !important;
-    text-align: center;
+  color: #ecf0f1;
+  font-size: 40px;
+  font-weight: regular;
+  font-family: "Roboto", serif;
+  margin: 2px;
+  margin-bottom: 0px !important;
+  text-align: center;
 }
 
-@media screen and (max-width:960px) {
-  .text{
+@media screen and (max-width: 960px) {
+  .text {
     font-size: 10px;
     font-weight: bold;
   }
 
-  .digit{
+  .digit {
     font-size: 30px;
     font-weight: lighter;
   }
