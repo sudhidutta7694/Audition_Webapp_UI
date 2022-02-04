76e08ddb38353fdc28cabf9777a243e281b09200
Diptangshu Dey <dipdeys.dey@gmail.com>
2022-02-05 01:09:14 +0530

    minor changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -115,6 +115,7 @@ export default {
     console.log(token)
     console.log(this.$route.query.token)
     console.log(this.$route.query)
+    console.log(this.$route)
     if (token != null) {
       token = "Bearer " + token;
       localStorage.setItem("token", token);
