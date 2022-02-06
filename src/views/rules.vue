4f71f81fdbeddda8646f7891c65edd5316ef0f9f
Diptangshu Dey <dipdeys.dey@gmail.com>
2022-02-06 18:30:56 +0530

    fixed faq and rules pages

--- src/views/rules.vue
+++ src/views/rules.vue
@@ -1,35 +1,8 @@
 <template>
     <div>
-        <Navigation :role="role" />
         <v-card width="80%">
             <div class="text-xl-h4 text-center">RULES</div>
         </v-card>
+        <router-link :to="{ name: 'Home' }">Back To Home</router-link>
     </div>
 </template>
-
-<script>
-import Navigation from '../components/Navigation.vue'
-import VueJwtDecode from 'vue-jwt-decode'
-
-export default {
-    data() {
-        return {
-            role: null,
-        }
-    },
-    components: {
-        Navigation,
-    },
-    created() {
-        this.$vuetify.theme.dark = true;
-        if (localStorage.getItem("token") === null) {
-            this.$router.push("/");
-        } else {
-            var tok = VueJwtDecode.decode(localStorage.getItem("token").substring(6));
-
-            this.role = tok.role;
-
-        }
-    },
-}
-</script>
